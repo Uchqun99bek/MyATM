20272bd83380e2d223536a4d04834590f0b6f657
E-MaxUser <https://github.com/Uchqun99bek/java.git>
2021-12-14 01:14:25 +0500

    egtvfddfdsf

--- src/main/java/uz/bek/Main.java
+++ src/main/java/uz/bek/Main.java
@@ -1,8 +1,42 @@
 package uz.bek;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
-        System.out.println("kkk");
+        try {
+            URL url=new URL("https://kun.uz/news/2021/12/13/kentukkidagi-tornado-hujumi-fotolar");
+            URLConnection urlConnection= url.openConnection();
+            BufferedReader reader=
+                    new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while ((line=reader.readLine())!=null){
+                System.out.println(line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            URL url1=new URL("https://kun.uz/news/2021/12/13");
+            URLConnection urlConnection=url1.openConnection();
+            BufferedReader reader=
+                    new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+
+            while ((line= reader.readLine())!=null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("hshshksahk\s");
+
     }
 }
